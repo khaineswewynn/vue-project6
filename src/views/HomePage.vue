0f2ce8d281c27961d3fdbf7f74abcb1e3a50422c
KhaingSweWin <khaingswewin@hostmyanmar.net>
2023-12-10 13:21:50 +0630

    added HomePage in views

---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2> Home Page</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "HomePage"
+    }
+</script>
+
+<style scoped>
+
+</style>
